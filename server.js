0d2c737e3ec1c3f096a9d4287e53abf8c419cce1
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-05 17:08:43 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/aricle-one', function(req,res){
+app.get('/aritcle-one', function(req,res){
     res.send("article one requested and it will be sent from here");
 });
-app.get('/aricle-two', function(req,res){
+app.get('/aritcle-two', function(req,res){
     res.send("article two requested and it will be sent from here");
 });
-app.get('/aricle-three', function(req,res){
+app.get('/aritcle-three', function(req,res){
     res.send("article three requested and it will be sent from here");
 });
 app.get('/ui/style.css', function (req, res) {
