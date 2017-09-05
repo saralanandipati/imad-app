bb73f3194f7ac5c9a32ce6f18893f4d4ebadea33
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-05 17:16:08 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/aritcle-one', function(req,res){
-    res.send("article one requested and it will be sent from here");
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/aritcle-two', function(req,res){
     res.send("article two requested and it will be sent from here");
