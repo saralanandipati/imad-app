6881e4445f940949ef56ace256e970d904ccb44a
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-05 17:50:26 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'Article one',
+    heading :'Article one',
+    date : 'sep 5th 2017',
+    content :
+    `<p>
+              This is aricle one content This is aricle one content This is aricle one content  This is aricle one content This is aricle one content
+          </p>
+          <p>
+              This is aricle one content This is aricle one content This is aricle one content 
+          </p>`
+          
+};
+
+function createTemplate(data){
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+var htmlTemplate = 
+  `<html>
+    <head>
+        <title>${title}</title>
+    </head>
+    <body>
+      <div>
+      <a href="/">home</a> 
+      </div>
+      <hr/>
+      <h3>${heading}</h3>
+      <div>
+      ${date}
+      </div>
+      <div>
+          ${content}
+      </div>
+        
+    </body>
+</html>`  
+;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
