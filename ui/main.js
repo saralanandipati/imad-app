9ba901b4652afb9b4aadcfb79c8ea279f0cbf8b3
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 07:07:12 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var button = documnet.getElementById("counter");
+var counter=0;
+button.onclick = function(){
+    
+    
+    counter = counter + 1; 
+    var span = document.getElementById("span");
+    span.innerHTML = counter.toString();
+};
