91616543dc7209a8baf8fb6ac46c0ae01cc14718
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 11:28:48 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ button.onclick = function(){
             
             if(request.state === 200){
                 var counter =request.responseText;
-                var span = document.getElementById("span");
+                var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
                 
             }
