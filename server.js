9ed943de529a7f7c642bc52dc73ac4e2296cf58c
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 18:34:39 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database : 'saralanandipati',
     host : 'db.imad.hasura-app.io',
     port : '5432',
-    password : process.env.DB-PASSWORD
+    password : process.env.DB_PASSWORD
     
     
 }
