7c2f4f8aa9cbbbaf26b52810f841b924e4cb02a8
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 11:26:51 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,26 @@
 console.log('Loaded!');
 
 var button = documnet.getElementById("counter");
-var counter=0;
+
 button.onclick = function(){
+    //create a reqest object
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            
+            if(request.state === 200){
+                var counter =request.responseText;
+                var span = document.getElementById("span");
+                span.innerHTML = counter.toString();
+                
+            }
+        }
+    }
     
-    counter = counter + 1; 
-    var span = document.getElementById("span");
-    span.innerHTML = counter.toString();
+    //make the request
+    
+    request.open('get', 'http://saralanandipati.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
