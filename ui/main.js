503346d9ca066dbb80df700d3947ddb7ca1e9eaf
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 11:50:34 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,17 @@ console.log('Loaded!');
 
 var button = documnet.getElementById("counter");
 
+alert("buttonvalue");
+
 button.onclick = function(){
     //create a reqest object
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
         if (request.readyState === XMLHttpRequest.DONE){
-            
+            alert("inside readystate");
             if(request.state === 200){
+                alert("inside readystate 200");
                 var counter = request.responseText;
                 var span = document.getElementById("count");
                 span.innerHTML = counter.toString();
