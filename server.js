5cc30cad0b2bf19a6fa93c0ccd0d3f8815cae63d
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 18:54:19 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,10 +125,21 @@ var names = [];
     res.send(JSON.stringify(names));
     
 });
-app.get('/:articleName', function(req,res){
+app.get('/articles/:articleName', function(req,res){
     //articleName === articleOne/articleTwo/articleThee
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    pool.query('SELECT * from article where title = '+ req.params.articleName, function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else if(result.rows.length === 0){
+          res.status(404).send('article not found');
+       }else{
+           var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+       }
+    });
+    
 });
 
 app.get('/ui/main.js', function (req, res) {
