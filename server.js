4f14f633c85d7d1a43113ecee6e53603fffe1d3b
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-07 01:18:25 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,37 @@ app.post('/create-user', function(req,res){
         
     });
 });
+
+app.post('/login', function(req,res){
+    
+    var userName = req.body.username;
+    var password = req.body.password;
+   
+    pool.query('SELECT * from "user" where username = $1', [userName], function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }else if(result.rows.length === 0){
+             res.setHeader('content-type','application/json');
+            res.send(JSON.parse('{"message": "username/password incorrect"}'));
+            }
+            else{
+                
+                var dbString = result.rows[0].password; 
+                var salt = dbString.split('$')[2];
+                var hashedPassword = hash(dbString,salt);
+                if(hashedPassword === dbString){
+                    req.session.auth = {userId : result.rows[0].id};
+                    res.setHeader('content-type','application/json');
+                    res.send(JSON.parse('{"message": "credentials are correct"}'));
+                }else{
+                    res.send(403).send("username/password is incorrect");
+                }
+           
+        }
+        
+    });
+});
 var counter=0;
 app.get('/counter', function(req,res){
     counter = counter+1;
