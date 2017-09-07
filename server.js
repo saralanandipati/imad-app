a491e232963b262b044de214d7164c2dc66736a9
saralanandipati <31021360+saralanandipati@users.noreply.github.com>
2017-09-06 19:02:30 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/articles/:articleName', function(req,res){
     //articleName === articleOne/articleTwo/articleThee
    // var articleName = req.params.articleName;
     
-    pool.query("SELECT * from article where title = '"+ req.params.articleName+"'", function(err,result){
+    pool.query("SELECT * from article where title = '" + req.params.articleName + "'", function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else if(result.rows.length === 0){
